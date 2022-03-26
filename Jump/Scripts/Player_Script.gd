00c7d7ab21bfa4ae968d1aae2f86a92987bcf330
Gleizits <98473712+Gleizits@users.noreply.github.com>
2022-03-25 21:39:55 -0500

    Add files via upload

---
+++ Jump/Scripts/Player_Script.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+const ACCELERATION = 70
+const MAX_SPEED = 300
+const JUMP_M = -900
+const UP = Vector2(0, -1)
+const gravity = 40
+onready var sprite = Sprite
+onready var animationPlayer = AnimationPlayer
+var motion = Vector2()
+func _physics_process(delta):
+	motion.y += gravity
+	var friction = false
+	if Input.is_action_pressed("ui_right"):
+		sprite.flip_h = true
+		animationPlayer.play("walk")
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+	elif Input.is_action_pressed("ui_left"):
+		animationPlayer.play("walk")
+		sprite.flip_h = false
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+	else:
+		friction = true
+		#animationPlayer.play("Idle")
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			motion.y = JUMP_M
+		if friction == true:
+			motion.x =lerp(motion.x,0,0.5)
+	else:
+		if friction == true:
+			motion.x = lerp(motion.x,0,0.01)
+	motion = move_and_slide(motion, UP)
